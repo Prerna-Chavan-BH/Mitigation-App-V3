3c5fe5a10f4e68373cc8aa7c287e0cfb93f33be0
Prerna Chavan <prerna.chavan@bakerhughes.com>
2025-01-27 14:43:09 +0530

    eighth commit

--- frontend/src/app/mitigation.forms/create-mitigation-dialog.component.ts
+++ frontend/src/app/mitigation.forms/create-mitigation-dialog.component.ts
@@ -1,35 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule, FormControl } from '@angular/forms';
 import { MitigationService } from '../mitigation.service';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-create-mitigation-dialog',
   templateUrl: './create-mitigation-dialog.component.html',
   styleUrls: ['./create.mitigation-dialog.component.css'],
-  imports: [ FormsModule, CommonModule, ReactiveFormsModule ]
+  imports: [ReactiveFormsModule, FormsModule, CommonModule]
 })
-
-export class CreateMitigationDialogComponent {
-  mitigationForm: FormGroup;
+export class CreateMitigationDialogComponent implements OnInit {
+  mitigationForm: FormGroup = new FormGroup({});
   tableData: any[] = [];
   error: string | undefined;
 
-  constructor(private fb: FormBuilder, private MitigationService: MitigationService) {
-    this.mitigationForm = this.fb.group({
-      title: ['', Validators.required],
-      description: ['', Validators.required],
-      status: ['', Validators.required],
-    });
-  }
+  constructor(private fb: FormBuilder, private MitigationService: MitigationService) { }
 
-  ngOnInit(): void{
-    this.mitigationForm = new FormGroup({
-      description: new FormControl('', Validators.required),
-      pre_mitigation_score: new FormControl('', Validators.required),
-      post_mitigation_score: new FormControl('', Validators.required),
-      applied_on: new FormControl('', Validators.required),
-    })
+  ngOnInit(): void {
+    this.mitigationForm = this.fb.group({
+      description: ['', Validators.required],
+      pre_mitigation_score: ['', Validators.required],
+      post_mitigation_score: ['', Validators.required],
+      applied_on: ['', Validators.required],
+
+    });
   }
 
   onSubmit(): void {
@@ -39,9 +34,10 @@ export class CreateMitigationDialogComponent {
         (response) => {
           // Add the new data to the tableData array for UI display
           this.tableData.push(response);
-          // Reset the form
-          this.mitigationForm.reset();
-        },(error) => {
+          // // Reset the form
+          // this.mitigationForm.reset();
+        },
+        (error) => {
           console.error('Error submitting form:', error);
           alert('Failed to submit form. Please try again.');
         }
@@ -50,4 +46,4 @@ export class CreateMitigationDialogComponent {
       alert('Please fill out all fields.');
     }
   }
-};
+}
